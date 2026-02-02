a95682879022e69301a87e71c67666f9ea7edc91
Chase Starr <chasestarr9@gmail.com>
2026-02-02 12:26:23 -0500

    connected database

--- server/db.js
+++ server/db.js
@@ -1,2 +1,25 @@
 const mongoose = require("mongoose");
-const mongodb = require("mongodb");
+require("dotenv").config({ path: "./config.env" });
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  });
+
+const hours = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  hoursWorked: {
+    type: Number,
+    required: true,
+  },
+});
+
+const hoursTrackerDB = mongoose.model("hoursTrackerDB", hours);
+
+module.exports = hoursTrackerDB;
