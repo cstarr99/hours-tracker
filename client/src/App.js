d9090bdcc8e9eb1b2e20ef1a629ea5f7f8e27dfa
Chase Starr <chasestarr9@gmail.com>
2026-02-06 17:06:57 -0500

    started working on react section

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hours Tracker</h1>
+      <WeeklyHours />
+    </div>
+  );
+}
+
+function WeeklyHours() {
+  const [hours, setHours] = useState(0);
+
+  return (
+    <div>
+      <h2>Weekly Hours</h2>
+      <span>Total Weekly Hours: {hours} </span>
+      <button onClick={() => setHours(hours + 1)}>Add Hour</button>
     </div>
   );
 }
