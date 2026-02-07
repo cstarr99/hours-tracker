6e02e0057a0063d8d72b21ba4800b04c399ba59c
Chase Starr <chasestarr9@gmail.com>
2026-02-07 10:27:09 -0500

    added areas for weekly/monthly hrs

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,55 @@ function App() {
     <div className="App">
       <h1>Hours Tracker</h1>
       <WeeklyHours />
+      <MonthlyHours />
     </div>
   );
 }
 
 function WeeklyHours() {
-  const [hours, setHours] = useState(0);
+  const [weeklyActHours, setWeeklyActHours] = useState(0);
+  const [weeklyPasHours, setWeeklyPasHours] = useState(0);
 
   return (
     <div>
       <h2>Weekly Hours</h2>
-      <span>Total Weekly Hours: {hours} </span>
-      <button onClick={() => setHours(hours + 1)}>Add Hour</button>
+      <span>Total Weekly Hours: {weeklyActHours + weeklyPasHours} </span>
+
+      <h2>Weekly Hours</h2>
+      <span>Total Weekly Active Hours: {weeklyActHours} </span>
+      <button onClick={() => setWeeklyActHours(weeklyActHours + 1)}>
+        Add Hour
+      </button>
+
+      <h2>Weekly Hours</h2>
+      <span>Total Weekly Passive Hours: {weeklyPasHours} </span>
+      <button onClick={() => setWeeklyPasHours(weeklyPasHours + 1)}>
+        Add Hour
+      </button>
+    </div>
+  );
+}
+
+function MonthlyHours() {
+  const [monthlyActHours, setMonthlyActHours] = useState(0);
+  const [monthlyPasHours, setMonthlyPasHours] = useState(0);
+
+  return (
+    <div>
+      <h2>Monthly Hours</h2>
+      <span>Total Weekly Hours: {monthlyActHours + monthlyPasHours} </span>
+
+      <h2>Monthly Hours</h2>
+      <span>Total Weekly Active Hours: {monthlyActHours} </span>
+      <button onClick={() => setMonthlyActHours(monthlyActHours + 1)}>
+        Add Hour
+      </button>
+
+      <h2>Monthly Hours</h2>
+      <span>Total Weekly Passive Hours: {monthlyPasHours} </span>
+      <button onClick={() => setMonthlyPasHours(monthlyPasHours + 1)}>
+        Add Hour
+      </button>
     </div>
   );
 }
